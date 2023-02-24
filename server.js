d12946eebd81c540daf3802418690489621c8c77
Piotr Sakowski <p.sakowski1213@gmail.com>
2023-02-24 12:33:29 +0100

    check database 'companyDB'

--- server.js
+++ server.js
@@ -12,6 +12,7 @@ mongoClient.connect('mongodb://localhost:27017/', { useNewUrlParser: true, useUn
   }
   else {
     console.log('Successfully connected to the database');
+    const db = client.db('companyDB');
     const app = express();
 
     app.use(cors());
