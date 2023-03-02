55dca1ceb48031292f2254a7e59e637c5f7fc737
Piotr Sakowski <p.sakowski1213@gmail.com>
2023-03-02 21:45:05 +0100

    fix task test & test:watch

---
+++ models/test/department.test.js
@@ -0,0 +1,15 @@
+const Department = require('../department.model.js');
+const expect = require('chai').expect;
+
+describe('Department', () => {
+
+    it('should throw an error if no "name" arg', () => {
+        const dep = new Department({}); // create new Department, but don't set `name` attr value
+      
+        dep.validate(err => {
+          expect(err.errors.name).to.exist;
+        });
+      
+      });
+  
+  });
