d582f3f4ff87b2e3ef5c7eacc14e5ae6c872014d
Piotr Sakowski <p.sakowski1213@gmail.com>
2023-03-05 19:40:53 +0100

    add onchange@6.1.0

---
+++ models/test/department.crud.test.js
@@ -0,0 +1,17 @@
+const Department = require('../department.model');
+const expect = require('chai').expect;
+const mongoose = require('mongoose');
+
+describe('Department', () => {
+  before(async () => {
+    try {
+      await mongoose.connect('mongodb://localhost:27017/companyDBtest', { useNewUrlParser: true, useUnifiedTopology: true });
+    } catch (err) {
+      console.error(err);
+    }
+  });
+
+  after(() => {
+    mongoose.models = {};
+  });
+});
